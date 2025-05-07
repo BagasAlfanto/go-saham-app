d228aebdca8359f308153efb470e697276c9da73
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-05-07 23:26:37 +0700

    chore(helpers): update some menu

--- helpers/console.go
+++ helpers/console.go
@@ -1,6 +1,8 @@
 package helpers
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ClearScreen() {
 	fmt.Print("\033[H\033[2J")
@@ -19,13 +21,32 @@ func DisplayAuthMenu() {
 	fmt.Println("=========================")
 }
 
-func DisplayMainMenu() {
+func DisplayMainMenu(username string) {
 	fmt.Println("===== Main Menu =====")
+	fmt.Printf("Hai, %s selamat datang!\n", username)
 	fmt.Println("1. Lihat Daftar Saham")
 	fmt.Println("2. Beli Saham")
 	fmt.Println("3. Lihat Profile")
 	fmt.Println("4. Logout")
 	fmt.Println("======================")
+}
+
+func DisplaySaham(){
+	fmt.Println("======= Saham =======")
+	fmt.Println("1. Lihat Semua Saham")
+	fmt.Println("2. Cari Saham")
+	fmt.Println("3. Urutkan dari harga terrendah")
+	fmt.Println("4. Urutkan dari harga tertinggi")
+	fmt.Println("5. Kembali Ke Menu Utama")
+	fmt.Println("======================")
+}
+
+func DisplayShowSaham(){
+	fmt.Println("===============================================================")
+	fmt.Println("|----------------------- Daftar Saham ------------------------|")
+	fmt.Println("===============================================================")
+	fmt.Printf("| %-15s %-30s %-12s |\n", "Kode Saham", "Nama Perusahaan", "Harga Saham")
+	fmt.Println("===============================================================")
 }
 
 func ConfirmationScreen(messages ...string) {
