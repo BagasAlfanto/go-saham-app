d24248b105a915ed3766b509632fe5e62d06dff7
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-05-07 23:25:23 +0700

    chore(controllers): update menu and add searching feature

--- controllers/SahamController.go
+++ controllers/SahamController.go
@@ -6,19 +6,69 @@ import (
 	"saham-app/model/saham"
 )
 
+func SahamMenu() {
+	var choice int
+
+	for isRunning := false; !isRunning; {
+		helpers.ClearScreen()
+		helpers.DisplaySaham()
+
+		for choice < 1 || choice > 5 {
+			fmt.Print("Masukan Pilihan Anda : ")
+			fmt.Scan(&choice)
+		}
+
+		switch choice {
+		case 1:
+			ShowSaham()
+		case 2:
+			SearchingSaham()
+		case 3:
+			saham.ShortAscending()
+		case 4:
+			saham.ShortDescending()
+		case 5:
+			isRunning = true
+		}
+
+		if choice != 5 {
+			choice = 0
+		}
+	}
+
+}
+
 func ShowSaham() {
 	var back string
+
 	helpers.ClearScreen()
-	fmt.Println("================ Daftar Saham ===============")
+	helpers.DisplayShowSaham()
+	
 	for _, daftarSaham := range saham.GetSaham() {
-		fmt.Printf("Nama Saham : %s\n", daftarSaham.NamaSaham)
-		fmt.Printf("Harga      : %d\n", daftarSaham.Harga)
-		fmt.Println("----------------------------------------------")
+		fmt.Printf("| %-15s %-30s %-12d |\n", daftarSaham.StockCode, daftarSaham.CompanyName, daftarSaham.Price_Per_Share)
 	}
+	fmt.Println("===============================================================")
+	for back != "ya" {
+		fmt.Println("Kembali ke menu utama? (ya)")
+		fmt.Scan(&back)
+		saham.UpdatePrice()
+		helpers.ClearScreen()
+	}
+}
+
+func SearchingSaham() {
+	var data, back string
+	fmt.Println("================== Cari Saham ==================")
+	fmt.Print("Masukan Kode atau Nama Perusahaan : ")
+	fmt.Scan(&data)
+
+	result := saham.Searching(data)
+	fmt.Println(result)
 
 	for back != "ya" {
 		fmt.Println("Kembali ke menu utama? (ya)")
 		fmt.Scan(&back)
+		saham.UpdatePrice()
 		helpers.ClearScreen()
 	}
 }
