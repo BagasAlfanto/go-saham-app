af34fdb9aebc4946e7a2a8f8dc37afa0de5ad6f1
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-05-16 10:33:38 +0700

    feat(model): add portofolio profile

---
+++ model/portofolio/model.go
@@ -0,0 +1,30 @@
+package portfolio
+
+import (
+	"saham-app/model/transaction"
+)
+
+/*
+ * Mendapatkan data saham dari user 
+ *
+ */
+func GetPortfolio(userID int) map[string]struct {
+	TotalLot   int
+	TotalModal int
+} {
+	result := make(map[string]struct {
+		TotalLot   int
+		TotalModal int
+	})
+
+	for _, t := range transaction.Transactions {
+		if t.UserID == userID {
+			val := result[t.NamaPerusahaan]
+			val.TotalLot += t.JumlahLot
+			val.TotalModal += t.Total
+			result[t.NamaPerusahaan] = val
+		}
+	}
+
+	return result
+}
