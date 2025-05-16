2c5bb1b82b12389dc4ecd5a7eeb0a8bcb76e01f1
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-05-16 10:29:34 +0700

    chore(handlers): update register system

--- handlers/auth/user.go
+++ handlers/auth/user.go
@@ -1,18 +1,49 @@
 package auth
 
 import (
-	"fmt"
+	"saham-app/helpers"
 	"saham-app/model/user"
 )
 
+/*
+ * Struct User
+ *
+ */
+type User struct {
+	ID       int
+	Username string
+	Password string
+	Saldo    int
+	Saham    []string
+}
+
+/*
+ * Variabel struct user & User account
+ *
+ */
+var Users []user.User
+var UserLogin *user.User
+
+/*
+ * Authtentic User
+ *
+ */
 func Authentic(username, password string) bool {
 	success, message := user.CheckPassword(username, password)
 
-	fmt.Println(message)
+	helpers.GetMessages(message)
 	return success
 }
 
+/*
+ * Register User
+ *
+ */
 func Register(username, password string) *user.User {
+
+	if user.IsUsernameExist(username) {
+		return nil
+	}
 	userData := user.User{
 		ID:       user.CreateID(),
 		Username: username,
