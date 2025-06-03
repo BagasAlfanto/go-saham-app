83921e4b05d49473d19a89ca59acddf7ff3f7c23
BagasFX <109197138+BagasFX@users.noreply.github.com>
2025-06-04 00:57:48 +0700

    chore(model): delete unimportant code

--- model/user/model.go
+++ model/user/model.go
@@ -2,7 +2,6 @@ package user
 
 import (
 	"saham-app/helpers"
-	"saham-app/model/transaction"
 )
 
 /*
@@ -139,29 +138,4 @@ func SaveUsers() {
 	if err != nil {
 		panic(err)
 	}
-}
-
-/*
- * Load daftar saham by user
- *
- */
-func GetPortfolio(userID int) map[string]struct {
-	TotalLot   int
-	TotalModal int
-} {
-	result := make(map[string]struct {
-		TotalLot   int
-		TotalModal int
-	})
-
-	for _, t := range transaction.Transactions {
-		if t.UserID == userID {
-			val := result[t.NamaPerusahaan]
-			val.TotalLot += t.JumlahLot
-			val.TotalModal += t.Total
-			result[t.NamaPerusahaan] = val
-		}
-	}
-
-	return result
 }
